9e3c80123d03bae6b13f7462e614710c06d0d99b
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-11-03 22:06:52 -0300

    criando o componente de Input

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,3 +1,16 @@
+import { Input } from "../components/input"
+
 export function SignIn() {
-  return <h1>SignIn</h1>
+  return (
+    <form action="" className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="Email"
+        type="email"
+        placeholder="SeuEmail@email.com"
+      />
+
+      <Input required legend="Senha" type="password" placeholder="S1234!" />
+    </form>
+  )
 }
