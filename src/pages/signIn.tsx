f561a6c2002f7a738049a1ea3a70896972981f31
Raphael-Steimvacher <raphaelSteimvacher2251@gmail.com>
2025-11-08 17:56:09 -0300

    useActionState

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,24 +1,28 @@
-import { useState } from "react"
+import { useActionState } from "react"
 
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+import { string } from "zod"
 
 export function SignIn() {
-  const [isLoading, setIsLoading] = useState(false)
+  const [state, formAction, isLoading] = useActionState(signIn, null)
 
-  function onAction(formData: FormData) {
-    // const email = formData.get("email")
-    console.log(formData.get("email"))
+  async function signIn(prevState: any, formData: FormData) {
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    return { email, password }
   }
 
   return (
-    <form action={onAction} className="w-full flex flex-col gap-4">
+    <form action={formAction} className="w-full flex flex-col gap-4">
       <Input
         name="email"
         required
         legend="E-mail"
         type="email"
         placeholder="SeuEmail@email.com"
+        defaultValue={String(state?.email)}
         autoComplete="username"
       />
 
@@ -28,6 +32,7 @@ export function SignIn() {
         legend="Senha"
         type="password"
         placeholder="S1234!"
+        defaultValue={String(state?.password)}
         autoComplete="current-password"
       />
 
